ed79949000b5c6c21578650616e211837c9c7d98
ayesha <fxolbird@gmail.com>
2021-10-17 04:31:49 -0700

    changes

---
+++ main.js
@@ -0,0 +1,10 @@
+let express = require("express");
+let app = express();
+
+app.get("/", (req,res) => {
+  res.send("welco to home page");
+});
+
+app.listen(3000, "127.0.0.1", ()=> {
+    console.log("listening to the port no 3000")
+});
